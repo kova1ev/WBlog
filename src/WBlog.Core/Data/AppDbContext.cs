fd02a15ec1a9c0d23e49fbce68220954edb2625b
kovalev <kov92av@gmail.com>
2022-03-19 21:22:00 +0300

    add dbContext Sqlite

---
+++ src/WBlog.Core/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WBlog.Domain.Entity;
+
+namespace WBlog.Core.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Post> Posts => Set<Post>();
+        public DbSet<Tag> Tags => Set<Tag>();
+    }
+}
