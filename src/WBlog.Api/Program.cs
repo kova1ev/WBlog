f8baa6f918e251dd154c035432c8dfa6da1fda8b
kovalev <kov92av@gmail.com>
2022-03-20 22:04:21 +0300

    fix swagger

--- src/WBlog.Api/Program.cs
+++ src/WBlog.Api/Program.cs
@@ -20,7 +20,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Wblog API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 else
 {
