d611966e3d17118fa63fb72055a35af35e10eed2
kovalev <kov92av@gmail.com>
2022-03-30 12:51:47 +0300

    added method in ITagRepo

--- src/WBlog.Core/Repository/Interface/ITagRepository.cs
+++ src/WBlog.Core/Repository/Interface/ITagRepository.cs
@@ -6,5 +6,9 @@ namespace WBlog.Core.Repository.Interface
     public interface ITagRepository
     {
         Task<IEnumerable<Tag>> GetAllTags();
+        Task<IEnumerable<Tag>> GetPopulasTags();
+        Task<bool> Remove(Guid id);
+        Task<bool> Add(Tag tag);
+        Task<bool> Update(Tag tag);
     }
 }
--- src/WBlog.Core/Repository/TagRepository.cs
+++ src/WBlog.Core/Repository/TagRepository.cs
@@ -8,5 +8,25 @@ namespace WBlog.Core.Repository
         {
             throw new NotImplementedException();
         }
+
+        public Task<IEnumerable<Tag>> GetPopulasTags()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Add(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
