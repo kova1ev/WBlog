197fb289aca1ef03132e2e61aa9406c0b1bd76b3
kovalev <kov92av@gmail.com>
2022-04-12 13:50:53 +0300

    added string extantions class

---
+++ src/WBlog.Infrastructure/Extantions/StringExtantions.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace WBlog.Infrastructure.Extantions
+{
+    public static class StringExtantions
+    {
+
+        public static string CreateHash(this string password, string salt)
+        {
+            byte[] saltBytes = Encoding.UTF8.GetBytes(salt);
+            byte[] hash = KeyDerivation.Pbkdf2(
+                            password: password,
+                            salt: saltBytes,
+                            prf: KeyDerivationPrf.HMACSHA256,
+                            iterationCount: 100000,
+                            numBytesRequested: 256 / 8);
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
--- src/WBlog.Infrastructure/Services/AdminService.cs
+++ src/WBlog.Infrastructure/Services/AdminService.cs
@@ -1,11 +1,10 @@
-using System.Text;
-using WBlog.Application.Core.Services;
-using WBlog.Application.Domain.Entity;
-using WBlog.Infrastructure.Data;
-using WBlog.Application.Core.Dto;
-using WBlog.Application.Core.Exceptions;
-using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.EntityFrameworkCore;
+using WBlog.Application.Domain.Entity;
+using WBlog.Application.Core.Services;
+using WBlog.Application.Core.Exceptions;
+using WBlog.Application.Core.Dto;
+using WBlog.Infrastructure.Data;
+using WBlog.Infrastructure.Extantions;
 
 namespace WBlog.Infrastructure.Services
 {
@@ -22,29 +21,17 @@ namespace WBlog.Infrastructure.Services
 
         public async Task<bool> Validation(LoginModel loginModel, string salt)
         {
-            string password = CreateHash(loginModel.Password!, salt);
-
+            if (string.IsNullOrWhiteSpace(loginModel.Password))
+                return false;
+            string password = loginModel.Password.CreateHash(salt);
             Admin? admin = await GetAdmin(loginModel.Email!);
             if (admin == null)
                 throw new ObjectNotFoundExeption($"Admin not found.");
-
             if (password == admin.Password)
                 return true;
             return false;
         }
 
-        // todo сделать норманый метод создаия с солью
-        public string CreateHash(string password, string salt)
-        {
-            byte[] saltBytes = Encoding.UTF8.GetBytes(salt);
-            byte[] hash = KeyDerivation.Pbkdf2(
-                            password: password,
-                            salt: saltBytes,
-                            prf: KeyDerivationPrf.HMACSHA256,
-                            iterationCount: 100000,
-                            numBytesRequested: 256 / 8);
-            return Convert.ToBase64String(hash);
-        }
 
         private async Task<Admin?> GetAdmin(string email)
         {
