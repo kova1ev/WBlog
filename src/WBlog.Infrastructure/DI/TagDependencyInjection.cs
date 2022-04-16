cea463417514cff21a79552c4bbd9a6083c51b1d
kovalev <kov92av@gmail.com>
2022-04-16 14:33:14 +0300

    add  Tag DependencyInjection class

---
+++ src/WBlog.Infrastructure/DI/TagDependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WBlog.Application.Core.Interfaces;
+using WBlog.Infrastructure.Data.Repository;
+using WBlog.Infrastructure.Services;
+
+namespace WBlog.Infrastructure.DI
+{
+    public static class TagDependencyInjection
+    {
+        public static void AddTagService(this IServiceCollection services)
+        {
+            services.AddScoped<ITagService, TagService>();
+        }
+        public static void AddTagRepository(this IServiceCollection services)
+        {
+            services.AddScoped<ITagRepository, TagRepository>();
+        }
+    }
+}
