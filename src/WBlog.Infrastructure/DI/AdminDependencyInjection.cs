c734218d5b5d8572d2431e3e7f7a22328733e9eb
kovalev <kov92av@gmail.com>
2022-04-16 14:32:56 +0300

    add Admin DependencyInjection class

---
+++ src/WBlog.Infrastructure/DI/AdminDependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WBlog.Application.Core.Interfaces;
+using WBlog.Infrastructure.Data.Repository;
+using WBlog.Infrastructure.Services;
+
+namespace WBlog.Infrastructure.DI
+{
+    public static class AdminDependencyInjection
+    {
+        public static void AddAdminService(this IServiceCollection services)
+        {
+            services.AddScoped<IAdminService, AdminService>();
+        }
+        public static void AddAdminRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IAdminRepository, AdminRepository>();
+        }
+    }
+}
