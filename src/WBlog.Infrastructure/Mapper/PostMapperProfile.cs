10a9891aa9bfd6042dc9ac637dcc1c1470b43636
kovalev <kov92av@gmail.com>
2022-04-16 14:50:05 +0300

    add Post Mapper Profile

---
+++ src/WBlog.Infrastructure/Mapper/PostMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using WBlog.Application.Core.Dto;
+using WBlog.Application.Domain.Entity;
+
+namespace WBlog.Infrastructure.Mapper
+{
+    public class PostMapperProfile : Profile
+    {
+        public PostMapperProfile()
+        {
+            //posts
+            CreateMap<Post, PostDetailsDto>()
+            .ForMember(output => output.Tags, src => src.MapFrom(p => p.Tags.Select(t => t.Name)));
+            CreateMap<Post, PostIndexDto>();
+        }
+
+    }
+}
