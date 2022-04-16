b717b04189e30c1a3a169d4a44e8c70873b88930
kovalev <kov92av@gmail.com>
2022-04-16 14:33:25 +0300

    add Post DependencyInjection

---
+++ src/WBlog.Infrastructure/DI/PostDependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WBlog.Application.Core.Interfaces;
+using WBlog.Infrastructure.Data.Repository;
+using WBlog.Infrastructure.Services;
+
+namespace WBlog.Infrastructure.DI
+{
+    public static class PostDependencyInjection
+    {
+        public static void AddPostService(this IServiceCollection services)
+        {
+            services.AddScoped<IPostService, PostService>();
+        }
+        public static void AddPostRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IPostRepository, PostRepository>();
+        }
+    }
+}
