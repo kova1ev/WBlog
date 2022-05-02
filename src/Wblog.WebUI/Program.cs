17677e225473b1816ad68904a9b53dd95e009c1e
kovalev <kov92av@gmail.com>
2022-05-02 16:21:53 +0300

    resize  back ground site pic

--- src/Wblog.WebUI/Program.cs
+++ src/Wblog.WebUI/Program.cs
@@ -1,12 +1,9 @@
-
-
-using System.Text.Json;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //builder.Services.AddRazorPages().AddNewtonsoftJson().AddRazorRuntimeCompilation();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 builder.Services.AddScoped(client => new HttpClient
 {
     BaseAddress = new Uri(builder.Configuration.GetSection("ServerUri").Value)
@@ -16,7 +13,7 @@ builder.Services.AddScoped(client => new HttpClient
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (@app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
