7d310b61a7c8ad696615d5beca0a0d3feecb31aa
kovalev <kov92av@gmail.com>
2022-05-04 20:10:32 +0300

    add in memory db in api

--- src/WBlog.Web.Api/Program.cs
+++ src/WBlog.Web.Api/Program.cs
@@ -11,9 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                    //options.JsonSerializerOptions.PropertyNamingPolicy = null;
-                    //options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-                    //options.JsonSerializerOptions.MaxDepth = 0;
                 });
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
@@ -37,16 +34,12 @@ builder.Services.AddPostService();
 builder.Services.AddTagService();
 builder.Services.AddAdminService();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
-});
-
 //builder.Services.AddDbContext<AppDbContext>(options =>
 //{
-//    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
-//    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 //});
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("memorydb"));
 
 builder.Services.AddAutoMapper(typeof(PostMapperProfile), typeof(TagsMapperProfile));
 
@@ -65,11 +58,6 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-//test innit data
-var dbcontext = app.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
-WBlog.Infrastructure.Data.SeedTestData.CreatdData(dbcontext);
-
-
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 
@@ -77,5 +65,9 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//test innit data
+var dbcontext = app.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
+WBlog.Infrastructure.Data.SeedTestData.CreatdData(dbcontext);
 
 app.Run();
