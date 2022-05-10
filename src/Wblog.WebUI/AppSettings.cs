1630c35b1f40b3e9bb8de08e729a659527cf50a5
kovalev <kov92av@gmail.com>
2022-05-10 19:46:43 +0300

    add configure settings

---
+++ src/Wblog.WebUI/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Wblog.WebUI
+{
+    public class AppSettings
+    {
+        public string? BlogUrl { get; set; }
+        public string GitHubLink { get; set; } = string.Empty;
+        public string TelegramLink { get; set; } = string.Empty;
+    }
+}
