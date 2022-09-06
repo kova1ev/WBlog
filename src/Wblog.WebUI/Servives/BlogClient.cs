62345ae8a2ea67c3e27287ddc621bec5b936419e
kovalev <kov92av@gmail.com>
2022-09-07 00:31:52 +0300

    remove useless method from blogservice

--- src/Wblog.WebUI/Servives/BlogClient.cs
+++ src/Wblog.WebUI/Servives/BlogClient.cs
@@ -9,7 +9,7 @@ namespace Wblog.WebUI.Servises
     {
         private readonly HttpClient _httpClient;
 
-        public BlogClient(IOptions<AppSettings> options, HttpClient httpClient)
+        public BlogClient(IOptions<SiteOptions> options, HttpClient httpClient)
         {
             _httpClient = httpClient ?? throw new ArgumentNullException($"{nameof(httpClient)}");
             _httpClient.BaseAddress = new Uri(options.Value.BaseAddress ?? throw new InvalidOperationException());
@@ -28,45 +28,5 @@ namespace Wblog.WebUI.Servises
             return resultData;
         }
 
-        public async Task<bool> DeleteAsync(string urlString)
-        {
-            HttpResponseMessage response = await _httpClient.DeleteAsync(urlString);
-
-            response.EnsureSuccessStatusCode();
-
-            var jsonString = await response.Content.ReadAsStringAsync();
-            bool result = JsonSerializer.Deserialize<bool>(jsonString);
-
-            return result;
-        }
-
-        public async Task<bool> PutAsync(string urlString, TagModel entity)
-        {
-            var tag = JsonSerializer.Serialize(entity);
-            var requestContent = new StringContent(tag, Encoding.UTF8, "application/json");
-
-            HttpResponseMessage response = await _httpClient.PutAsync(urlString, requestContent);
-            var content = await response.Content.ReadAsStringAsync();
-            response.EnsureSuccessStatusCode();
-            // if (!response.IsSuccessStatusCode)
-            // {
-            //     throw new ApplicationException($"{response.ReasonPhrase} {content}");
-            // }
-
-            var jsonString = await response.Content.ReadAsStringAsync();
-            bool result = JsonSerializer.Deserialize<bool>(jsonString);
-
-            return result;
-        }
-
-        public async Task<bool> PublishAsync(string urlString)
-        {
-            HttpResponseMessage response = await _httpClient.PutAsync(urlString, null);
-            response.EnsureSuccessStatusCode();
-
-            var jsonString = await response.Content.ReadAsStreamAsync();
-            bool result = JsonSerializer.Deserialize<bool>(jsonString);
-            return result;
-        }
     }
 }
--- src/Wblog.WebUI/Servives/IBlogClient.cs
+++ src/Wblog.WebUI/Servives/IBlogClient.cs
@@ -5,8 +5,5 @@ namespace Wblog.WebUI.Servises
     public interface IBlogClient
     {
         public Task<T> GetAsync<T>(string urlString) where T : class;
-        public Task<bool> PutAsync(string urlString, TagModel entity);
-        public Task<bool> DeleteAsync(string urlString);
-        public Task<bool> PublishAsync(string urlString);
     }
 }
