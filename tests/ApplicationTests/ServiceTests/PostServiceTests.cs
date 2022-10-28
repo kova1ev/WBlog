bc9f4a0b6b32c1d87aced31466ed4c6da7feff0e
kovalev <kov92av@gmail.com>
2022-10-29 00:06:48 +0300

    add test query string for post

--- tests/ApplicationTests/ServiceTests/PostServiceTests.cs
+++ tests/ApplicationTests/ServiceTests/PostServiceTests.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WBlog.Core;
+using WBlog.Core.Domain.Entity;
 using WBlog.Core.Interfaces;
 using WBlog.Core.Services;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace ApplicationTests.ServiceTests
 {
@@ -16,7 +18,7 @@ namespace ApplicationTests.ServiceTests
 
 
         [Fact]
-        public async Task Get_post()
+        public async Task Get_publish_post()
         {
             ArticleRequestOptions options = new ArticleRequestOptions { Publish = true };
             //arrage
@@ -30,5 +32,24 @@ namespace ApplicationTests.ServiceTests
             //assert
             Assert.True(result.Data.All(p => p.IsPublished == true));
         }
+
+        [Theory]
+        [InlineData("pro")]
+        [InlineData("BLA")]
+        public async Task Get_post_by_querystring(string query)
+        {
+            ArticleRequestOptions options = new ArticleRequestOptions { Query = query };
+
+            var mockPost = new Mock<IPostRepository>();
+            mockPost.Setup(repo => repo.Posts).Returns(memoryDb.Posts);
+            var mockTag = new Mock<ITagRepository>();
+            IPostService postService = new PostService(mockPost.Object, mockTag.Object);
+
+            var result = await postService.GetPosts(options);
+
+            Assert.True(result.Data.All(p => p.Title.ToLower().Contains(query.ToLower())));
+        }
+
+
     }
 }
